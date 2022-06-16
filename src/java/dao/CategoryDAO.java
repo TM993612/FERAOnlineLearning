ec78de984219efb52aefde10cb5584baf7e7abcb
kiendthe141156 <kiendthe141156@fpt.edu.vn>
2022-06-16 18:01:16 +0700

    test

--- src/java/dao/CategoryDAO.java
+++ src/java/dao/CategoryDAO.java
@@ -3,14 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dao;
+package DAO;
 
-import context.DBContext;
+import DBcontext.DBContext;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import ultities.Category;
 
 /**
  *
- * @author Tra My
+ * @author Dell
  */
-public class CategoryDAO extends DBContext{
-    
+public class CategoryDAO {
+
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private String query;
+
+    public ArrayList<Category> getCategories() {
+        ArrayList<Category> list = new ArrayList<>();
+        query = "SELECT * FROM dbo.Category";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Category(rs.getInt("id"), 
+                        rs.getString("Name")
+                ));
+            }
+
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
 }
